ee444c6bbc57617d51ea4d60de7320af2f5cfb19
bintangyudhis <bintangyudhis333@gmail.com>
2025-10-08 12:59:46 +0700

    pembuatan model dan migration subcription

---
+++ app/Models/Subscription.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Subscription extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'end_date',
+        'is_active',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Transaction::class);
     }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class);
+    }
 }
