3223a90628a0049142d47ebec5e960dd658bb38f
bintangyudhis <bintangyudhis333@gmail.com>
2025-10-13 22:42:21 +0700

    penambahan boot/generate slug otomatis beserta user id nya

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+
+use App\Models\Product;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -16,12 +20,31 @@ class ProductCategory extends Model
         'slug', // slug -> id unik ketika kita melakukan filter brdasarkan product category slug ini yang akan dimunculkan daripada kita menggunakan id
     ];
 
+    public static function boot() // melakukan generate slug otomatis
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (Auth::user()->role == 'store') {
+                $model->user_id = Auth::user()->id; // ketika sedang create otomatis menginjek si user id nya beserta slugnya
+            }
+
+            $model->slug = str()->slug($model->name); // generate slug dari name
+        });
+
+        static::updating(function ($model) {
+            if (Auth::user()->role == 'store') {
+                $model->user_id = Auth::user()->id; // ketika sedang update otomatis menginjek si user id nya beserta slugnya
+            }
+            $model->slug = str()->slug($model->name); // generate slug dari name
+        });
+    }
     public function user()
     {
         return $this->belongsTo(User::class); // relasi
     }
 
-     public function products() // 1 productcategory bisa memiliki banyak product
+    public function products() // 1 productcategory bisa memiliki banyak product
     {
         return $this->hasMany(Product::class);
     }
