1e3c333c3faf855ef9e32f46d45967ee9fc38ae2
bintangyudhis <bintangyudhis333@gmail.com>
2025-10-18 22:45:07 +0700

    menambahkan beberapa column, filter dan action

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -2,13 +2,21 @@
 
 namespace App\Filament\Resources\Products\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
-use Filament\Actions\ForceDeleteBulkAction;
-use Filament\Actions\RestoreBulkAction;
-use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use App\Models\ProductCategory;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Actions\DeleteAction;
+use Illuminate\Support\Facades\Auth;
+use Filament\Actions\BulkActionGroup;
+use Filament\Schemas\Components\View;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Actions\ForceDeleteBulkAction;
 
 class ProductsTable
 {
@@ -16,13 +24,41 @@ class ProductsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('user.name')
+                    ->label('Nama Toko')
+                    ->hidden(fn() => Auth::user()->role == 'store'), // jika user role nya store maka tidak munculin nama toko
+                TextColumn::make('productCategory.name') // munculin kategori menu
+                    ->label('Kategori Menu'),
+                ImageColumn::make('image') // munculin image
+                    ->label('Foto Menu'),
+                TextColumn::make('price') // munculin harga
+                    ->label('Harga Menu')
+                    ->formatStateUsing(function (string $state) {
+                        return 'RP ' . number_format($state);
+                    }),
             ])
             ->filters([
                 TrashedFilter::make(),
+
+                SelectFilter::make('user')
+                    ->relationship('user', 'name')
+                    ->label('Toko')
+                    ->hidden(fn() => Auth::user()->role == 'store'),
+
+                SelectFilter::make('product_category_id')
+                    ->options(function () {
+                        if (Auth::user()->role === 'admin') {
+                            return ProductCategory::pluck('name', 'id');
+                        }
+                        return ProductCategory::where('user_id', Auth::user()->id)
+                            ->pluck('name', 'id');
+                    })
+                    ->label('Kategori Menu'),
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
