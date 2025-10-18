b8ad6a9029550318ecea44508ceeb4aec5499c7e
bintangyudhis <bintangyudhis333@gmail.com>
2025-10-18 22:43:47 +0700

    menambahkan untuk membatasi hanya admin yang bisa melihat semua data klo bukan admin maka di filter sesui user id

--- app/Filament/Resources/Products/ProductResource.php
+++ app/Filament/Resources/Products/ProductResource.php
@@ -2,20 +2,21 @@
 
 namespace App\Filament\Resources\Products;
 
-use App\Filament\Resources\Products\Pages\CreateProduct;
-use App\Filament\Resources\Products\Pages\EditProduct;
-use App\Filament\Resources\Products\Pages\ListProducts;
-use App\Filament\Resources\Products\Schemas\ProductForm;
-use App\Filament\Resources\Products\Tables\ProductsTable;
-use App\Models\Product;
-use BackedEnum;
 use UnitEnum;
-use Filament\Resources\Resource;
-use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
+use BackedEnum;
+use App\Models\Product;
 use Filament\Tables\Table;
+use Filament\Schemas\Schema;
+use Filament\Resources\Resource;
+use Filament\Support\Icons\Heroicon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\Products\Pages\EditProduct;
+use App\Filament\Resources\Products\Pages\ListProducts;
+use App\Filament\Resources\Products\Pages\CreateProduct;
+use App\Filament\Resources\Products\Schemas\ProductForm;
+use App\Filament\Resources\Products\Tables\ProductsTable;
 
 class ProductResource extends Resource
 {
@@ -26,6 +27,17 @@ class ProductResource extends Resource
     protected static ?string $navigationLabel = 'Manajemen Produk';
 
     protected static string | UnitEnum |null $navigationGroup = 'Manjemen Menu';
+
+    public static function getEloquentQuery(): Builder
+    {
+        $user = Auth::user(); // panggil user yang sedang login
+
+        if ($user->role === 'admin') {
+            return parent::getEloquentQuery(); // kalau user role nya admin  dia akan menampilkan semua data yang ada
+        }
+
+        return parent::getEloquentQuery()->where('user_id', $user->id); // ketika selain admin dia akan melakukan filter berdasarkan user id
+    }
 
     public static function form(Schema $schema): Schema
     {
