ad0b4bdc258550a92b4800d240573280faaf58f4
bintangyudhis <bintangyudhis333@gmail.com>
2025-10-22 14:30:29 +0700

    penambahan generate otomatis slug pada model transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -18,6 +19,24 @@ class Transaction extends Model
         'total_price',
         'status',
     ];
+
+    public static function boot() // melakukan generate slug otomatis
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (Auth::user()->role == 'store') {
+                $model->user_id = Auth::user()->id; // ketika sedang create otomatis menginjek si user id nya beserta slugnya
+            }
+        });
+
+        static::updating(function ($model) {
+            if (Auth::user()->role == 'store') {
+                $model->user_id = Auth::user()->id; // ketika sedang update otomatis menginjek si user id nya beserta slugnya
+            }
+            // $model->slug = str()->slug($model->name); // generate slug dari name
+        });
+    }
 
     public function user()
     {
