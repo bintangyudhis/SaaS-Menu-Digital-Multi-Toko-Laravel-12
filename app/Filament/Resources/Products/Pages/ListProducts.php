2e2801c105a8472c9db2ff158ab230d9c25376da
bintangyudhis <bintangyudhis333@gmail.com>
2025-10-26 17:17:26 +0700

    penambahan alert ketika prodcut mencapai 5 dan belum berlangganan

--- app/Filament/Resources/Products/Pages/ListProducts.php
+++ app/Filament/Resources/Products/Pages/ListProducts.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\Products\Pages;
 
-use App\Filament\Resources\Products\ProductResource;
+use App\Models\Product;
+use App\Models\Subscription;
 use Filament\Actions\CreateAction;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\Products\ProductResource;
+use Filament\Actions\Action;
 
 class ListProducts extends ListRecords
 {
@@ -12,8 +16,28 @@ class ListProducts extends ListRecords
 
     protected function getHeaderActions(): array
     {
+        if (Auth::user()->role === 'admin') {
+            return [
+                CreateAction::make(),
+            ];
+        }
+
+        $subscription = Subscription::where('user_id', Auth::user()->id)
+            ->where('end_date', '>', now())
+            ->where('is_active', true)
+            ->latest()
+            ->first();
+
+        $countProduct = Product::where('user_id', Auth::user()->id)->count();
+
         return [
-            CreateAction::make(),
+            Action::make('alert')
+                ->label('Produk Kamu Melebihi Batas Penggunaan Gratis, Silahkan Berlangganan')
+                ->color('danger')
+                ->icon('heroicon-s-exclamation-triangle')
+                ->visible(!$subscription && $countProduct >= 5),
+            CreateAction::make()
+                ->visible($countProduct < 5 || $subscription),
         ];
     }
 }
