3990923c7e489271a7a0cb80ae859014d82436a9
bintangyudhis <bintangyudhis333@gmail.com>
2025-11-19 23:50:36 +0700

    membuat router untuk ke frontend nya

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// daftarkan controller ke router
+Route::get('/{username}', [FrontendController::class, 'index'])->name('index'); //memanggil frontendcontrooller dan memanggil fungsi index
