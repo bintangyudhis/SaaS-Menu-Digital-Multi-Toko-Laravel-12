db3f91083c26f1c219a1cc6b76118c81c088ee6c
bintangyudhis <bintangyudhis333@gmail.com>
2026-01-11 19:17:17 +0700

    penambahan route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
-use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\TransactionController;
 
 // daftarkan controller ke router
 Route::get('/{username}', [FrontendController::class, 'index'])->name('index'); //memanggil frontendcontrooller dan memanggil fungsi index
+
+// route  //username toko              // class controller      //fungsi        // nama
+Route::get('/{username}/find-product', [ProductController::class, 'find'])->name('product.find');
+Route::get('/{username}/find-product/result', [ProductController::class, 'findResults'])->name('product.find-results');
+Route::get('/{username}/product/{id}', [ProductController::class, 'show'])->name('product.show');
+
+Route::get('/{username}/cart', [TransactionController::class, 'cart'])->name('cart');
